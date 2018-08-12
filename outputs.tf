4bc266468f92cce8087100d4c862b61c3ca3b660
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2018-08-12 17:25:58 +0200

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "id" {
-  value = "${azurerm_template_deployment.resource.outputs["id"]}"
+  value = "${lookup(azurerm_template_deployment.resource.outputs, "id", "")}"
+}
+
+output "template_deployment_id" {
+  value = "${azurerm_template_deployment.resource.id}"
 }
