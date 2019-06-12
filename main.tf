5e244e8a61affb8be5ec713496573fa7ab9a63d0
Luca Pinna <>
2019-06-12 10:06:36 +0200

    add workaround for negative numbers'

--- main.tf
+++ main.tf
@@ -64,12 +64,14 @@ DEPLOY
     "location"   = "${var.location}"
     "name"       = "${var.name}"
     "plan"       = "${jsonencode(var.plan)}"
+
     # Jsonencode bug when working with numeric values
     # Details here https://github.com/hashicorp/terraform/issues/17033
     # Fixed in terraform 0.12
     # "properties" = "${jsonencode(var.properties)}"
-    "properties" = "${replace(replace("${jsonencode(var.properties)}","/\"(true|false|[[:digit:]]+)\"/", "$1"), "string:", "")}"
-    "sku"        = "${jsonencode(var.sku)}"
-    "tags"       = "${jsonencode(var.tags)}"
+    "properties" = "${replace(replace("${jsonencode(var.properties)}","/\"(true|false|[[:digit:]]+|-[[:digit:]]+)\"/", "$1"), "string:", "")}"
+
+    "sku"  = "${jsonencode(var.sku)}"
+    "tags" = "${jsonencode(var.tags)}"
   }
 }
