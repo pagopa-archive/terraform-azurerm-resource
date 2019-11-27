5512011b26cef2f6a8476314e817abd1be462fef
Roberto Chessa <r.chessa@beetobit.com>
2019-11-27 16:13:49 +0100

    remove \ from string

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "azurerm_template_deployment" "resource" {
             "type": "string"
         },
         "name": {
-            "type": "string"\
+            "type": "string"
         },
         "plan": {
             "type": "string"
